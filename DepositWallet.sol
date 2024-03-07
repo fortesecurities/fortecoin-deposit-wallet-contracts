3df72db08d4fa49f8285e3889b049d245f290762
Quentin Adam <quentinadam@gmail.com>
2024-03-07 11:26:14 +1100

    Simplify DepositWallet

--- DepositWallet.sol
+++ DepositWallet.sol
@@ -5,35 +5,19 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DepositWallet {
     
-    address public immutable ADMIN;
-    IERC20 public immutable TOKEN;
     address public immutable SWEEP_ADDRESS;
 
-    constructor(address admin, IERC20 token, address sweepAddress) {
-        ADMIN = admin;
-        TOKEN = token;
+    error ZeroBalance();
+
+    constructor(address sweepAddress) {
         SWEEP_ADDRESS = sweepAddress;
     }
 
-    function balance() view public returns (uint256) {
-        return TOKEN.balanceOf(address(this));
-    }
-
-    function sweep() public {
-        sweep(TOKEN.balanceOf(address(this)));
-    }
-
-    function sweep(uint256 amount) public {
-        TOKEN.transfer(SWEEP_ADDRESS, amount);
-    }
-
-    function transfer(IERC20 token, address to, uint256 amount) public {
-        require(msg.sender == ADMIN, "DepositWallet: sender not admin");
-        token.transfer(to, amount);
-    }
-
-    function transfer(address payable to, uint256 amount) public {
-        require(msg.sender == ADMIN, "DepositWallet: sender not admin");
-        to.transfer(amount);
+    function sweep(IERC20 token) public {
+        uint256 balance = token.balanceOf(address(this));
+        if (balance == 0) {
+            revert ZeroBalance();
+        }
+        token.transfer(SWEEP_ADDRESS, balance);
     }
 }
